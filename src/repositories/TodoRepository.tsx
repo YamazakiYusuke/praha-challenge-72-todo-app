2c45ee1cb29a890840487fc25c1ef19ddec1237d
YamazakiYusuke <yusuke38098@gmail.com>
2025-04-11 09:34:14 +0900

    deploy準備

--- src/repositories/TodoRepository.tsx
+++ src/repositories/TodoRepository.tsx
@@ -1,30 +1,33 @@
-import ITodoRepository from './ITodoRepository';
-import TodoItem from './TodoItem.tsx';
+// import ITodoRepository from './ITodoRepository';
+// import TodoItem from './TodoItem.tsx';
 
-class TodoRepository implements ITodoRepository {
-  private baseUrl: string;
+// class TodoRepository implements ITodoRepository {
+//   private baseUrl: string;
 
-  constructor() {
-    this.baseUrl = '/api';
-  }
+//   constructor() {
+//     this.baseUrl = '/api';
+//   }
 
-  async getAll(): Promise<TodoItem[]> {
-    const response = await fetch(`${this.baseUrl}/todos`);
-    const todos = await response.json();
-    return todos.map(todo => TodoItem.fromJSON(todo));
-  }
+//   async getAll(): Promise<TodoItem[]> {
+//     const response = await fetch(`${this.baseUrl}/todos`);
+//     const todos = await response.json();
+//     return todos.map(todo => TodoItem.fromJSON(todo));
+//   }
 
-  async save(todos: TodoItem[]): Promise<TodoItem[]> {
-    const response = await fetch(`${this.baseUrl}/todos`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(todos.map(todo => todo.toJSON())),
-    });
-    const savedTodos = await response.json();
-    return savedTodos.map(todo => TodoItem.fromJSON(todo));
-  }
-}
+//   async save(todos: TodoItem[]): Promise<TodoItem[]> {
+//     const response = await fetch(`${this.baseUrl}/todos`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(todos.map(todo => todo.toJSON())),
+//     });
+//     const savedTodos = await response.json();
+//       return savedTodos.map(todo => TodoItem.fromJSON(todo));
+//     } else {
+//       return [TodoItem.fromJSON(savedTodos)];
+//     }
+//   }
+// }
 
-export default TodoRepository; 
+// export default TodoRepository; 
