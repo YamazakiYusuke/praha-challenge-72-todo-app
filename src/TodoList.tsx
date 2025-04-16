04fb5634c41d235319d912a60e41259ad0ac856f
YamazakiYusuke <yusuke38098@gmail.com>
2025-04-16 09:05:47 +0900

    コンポーネントの適切な分割

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,9 +1,9 @@
-import { Delete as DeleteIcon, Edit as EditIcon } from '@mui/icons-material';
-import { Button, Checkbox, ClickAwayListener, Container, IconButton, List, ListItem, ListItemIcon, ListItemText, Paper, TextField } from '@mui/material';
+import { Container, Paper } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import TodoForm from './components/todo/TodoForm.tsx';
+import TodoItems from './components/todo/TodoItems.tsx';
 import TodoListLogic from './TodoListLogic.ts';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2),
@@ -11,12 +11,14 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
   maxWidth: 400,
 }));
 
+/**
+ * Todoリスト管理コンポーネント
+ */
 function TodoList() {
   const [todos, setTodos] = useState<Array<{ id: number; text: string; completed: boolean }>>([]);
   const [newTodo, setNewTodo] = useState('');
   const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
   const [editText, setEditText] = useState('');
-  const textFieldRef = useRef<HTMLInputElement>(null);
 
   const todoListLogic = useRef(new TodoListLogic()).current;
 
@@ -27,12 +29,6 @@ function TodoList() {
     };
     initializeTodos();
   }, []);
-
-  useEffect(() => {
-    if (editingTodoId !== null && textFieldRef.current) {
-      textFieldRef.current.focus();
-    }
-  }, [editingTodoId]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setNewTodo(event.target.value);
@@ -78,77 +74,22 @@ function TodoList() {
   return (
     <Container maxWidth="sm">
       <StyledPaper>
-        <TextField
-          fullWidth
-          label="Add Todo"
-          value={newTodo}
-          onChange={handleInputChange}
-          margin="normal"
-          onKeyDown={(event) => {
-            if (event.key === 'Enter') {
-              handleAddTodo();
-            }
-          }}
+        <TodoForm
+          newTodo={newTodo}
+          onInputChange={handleInputChange}
+          onAdd={handleAddTodo}
         />
-        <Button variant="contained" color="primary" onClick={handleAddTodo}>
-          Add
-        </Button>
-        <List>
-          {[
-            ...todos.filter((todo) => !todo.completed),
-            ...todos.filter((todo) => todo.completed),
-          ].map((todo) => (
-            <ListItem
-              key={todo.id}
-              secondaryAction={
-                <div>
-                  {editingTodoId !== todo.id ? (
-                    <IconButton
-                      edge="end"
-                      aria-label="edit"
-                      onClick={() => handleEditTodo(todo.id, todo.text)}
-                    >
-                      <EditIcon />
-                    </IconButton>
-                  ) : null}
-                  <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteTodo(todo.id)}>
-                    <DeleteIcon />
-                  </IconButton>
-                </div>
-              }
-            >
-              <ListItemIcon>
-                <Checkbox
-                  checked={todo.completed}
-                  onChange={() => handleToggleComplete(todo.id)}
-                />
-              </ListItemIcon>
-              {editingTodoId === todo.id ? (
-                <ClickAwayListener onClickAway={() => handleCancelEdit()}>
-                  <TextField
-                    fullWidth
-                    value={editText}
-                    onChange={(e) => setEditText(e.target.value)}
-                    onKeyDown={(e) => {
-                      if (e.key === 'Enter') {
-                        handleSaveTodo(todo.id);
-                      }
-                    }}
-                    inputRef={textFieldRef}
-                  />
-                </ClickAwayListener>
-              ) : (
-                <ListItemText
-                  primary={todo.text}
-                  style={{
-                    textDecoration: todo.completed ? 'line-through' : 'none',
-                    color: todo.completed ? '#888888' : 'inherit'
-                  }}
-                />
-              )}
-            </ListItem>
-          ))}
-        </List>
+        <TodoItems
+          todos={todos}
+          editingTodoId={editingTodoId}
+          editText={editText}
+          onToggleComplete={handleToggleComplete}
+          onDelete={handleDeleteTodo}
+          onEdit={handleEditTodo}
+          onSave={handleSaveTodo}
+          onCancel={handleCancelEdit}
+          onEditTextChange={setEditText}
+        />
       </StyledPaper>
     </Container>
   );
